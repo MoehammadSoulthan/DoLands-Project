47ace1664e71a1aa8c381c814e0091bf8b285354
raisadwinda <raisadwindah@gmail.com>
2021-04-02 16:09:51 +0700

    update

--- DoLands/app/src/main/java/id/ac/umn/dolands/ExploreActivity.java
+++ DoLands/app/src/main/java/id/ac/umn/dolands/ExploreActivity.java
@@ -7,11 +7,19 @@ import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -28,12 +36,48 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class ExploreActivity extends AppCompatActivity {
     SupportMapFragment supportMapFragment;
     FusedLocationProviderClient client;
+    ImageView ivAttract1,ivAttract2,ivAttract3,ivAttract4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
+        ivAttract1 = (ImageView) findViewById(R.id.attract1);
+        ivAttract2 = (ImageView) findViewById(R.id.attract2);
+        ivAttract3 = (ImageView) findViewById(R.id.attract3);
+        ivAttract4 = (ImageView) findViewById(R.id.attract4);
 
+        //intent ke detail
+        ivAttract1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExploreActivity.this, ExploreDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+        ivAttract2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExploreActivity.this, ExploreDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+        ivAttract3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExploreActivity.this, ExploreDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+        ivAttract4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExploreActivity.this, ExploreDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //maps
         supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.current_map);
         client = LocationServices.getFusedLocationProviderClient(this);
 
@@ -44,6 +88,7 @@ public class ExploreActivity extends AppCompatActivity {
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
         }
 
+        //menu bottom
         BottomNavigationView bottomNavigationView = findViewById(R.id.navBottom);
 
         bottomNavigationView.setSelectedItemId(R.id.nav_explore);
@@ -52,8 +97,32 @@ public class ExploreActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.nav_profile:
-                        startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
-                        overridePendingTransition(0,0);
+                        //popup alert login
+                        Dialog popup = new Dialog(ExploreActivity.this);
+                        popup.setContentView(R.layout.popup_login);
+
+                        popup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                        popup.setCancelable(false);
+
+                        Button btn_ok = popup.findViewById(R.id.button_ok);
+                        TextView tv_skip = popup.findViewById(R.id.text_skip);
+
+                        btn_ok.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void  onClick(View v) {
+                                startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+                                overridePendingTransition(0,0);
+                            }
+                        });
+
+                        tv_skip.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void  onClick(View v) {
+                                bottomNavigationView.setSelectedItemId(R.id.nav_explore);
+                                popup.dismiss();
+                            }
+                        });
+                        popup.show();
                         return true;
                     case R.id.nav_explore:
                         return true;
@@ -63,6 +132,7 @@ public class ExploreActivity extends AppCompatActivity {
         });
     }
 
+    //maps
     private void getCurrentLocation() {
         @SuppressLint("MissingPermission") Task<Location> task = client.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
@@ -84,6 +154,7 @@ public class ExploreActivity extends AppCompatActivity {
         });
     }
 
+    //maps
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == 44){
--- DoLands/app/src/main/java/id/ac/umn/dolands/ExploreDetailActivity.java
+++ DoLands/app/src/main/java/id/ac/umn/dolands/ExploreDetailActivity.java
@@ -5,17 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.text.Html;
 
-public class EditProfileActivity extends AppCompatActivity {
+public class ExploreDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile);
+        setContentView(R.layout.activity_explore_detail);
 
         //settingan action bar
-//        getSupportActionBar().setTitle(Html.fromHtml("<font color=\"yellow\">"));
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
         getSupportActionBar().setElevation(0);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back);
