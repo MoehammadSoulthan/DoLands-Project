5869c14bbb251bff8b97e4fc4dfcb0b7d55ffb78
MoehammadSoulthan <moehammad.soulthan@student.umn.ac.id>
2021-04-14 11:21:37 +0700

    Update Splashscreen

--- DoLands/app/src/main/java/id/ac/umn/dolands/SplashActivity.java
+++ DoLands/app/src/main/java/id/ac/umn/dolands/SplashActivity.java
@@ -2,19 +2,30 @@ package id.ac.umn.dolands;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DISPLAY_TIME = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // langsung pindah ke MainActivity atau activity lain
-        // begitu memasuki splash screen ini
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent();
+                intent.setClass(SplashActivity.this,
+                        MainActivity.class);
+
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_TIME);
     }
 }
